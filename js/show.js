f821cb8cf11c1e365bd11a5a97138195ee2aad2b
zdsndu <140469420+zdsndu@users.noreply.github.com>
2023-07-26 18:51:43 +0800

    Add files via upload

---
+++ js/show.js
@@ -0,0 +1,38 @@
+function show1() {
+	var show1 = document.getElementById("show1");
+	var show2 = document.getElementById("show2");
+	var show3 = document.getElementById("show3");
+	if (show1.style.display == "none") {
+		show1.style.display = "block";
+	} else {
+		show1.style.display = "none";
+	}
+	show2.style.display = "none";
+	show3.style.display = "none";
+}
+
+function show2() {
+	var show1 = document.getElementById("show1");
+	var show2 = document.getElementById("show2");
+	var show3 = document.getElementById("show3");
+	if (show2.style.display == "none") {
+		show2.style.display = "block";
+	} else {
+		show2.style.display = "none";
+	}
+	show1.style.display = "none";
+	show3.style.display = "none";
+}
+
+function show3() {
+	var show1 = document.getElementById("show1");
+	var show2 = document.getElementById("show2");
+	var show3 = document.getElementById("show3");
+	if (show3.style.display == "none") {
+		show3.style.display = "block";
+	} else {
+		show3.style.display = "none";
+	}
+	show2.style.display = "none";
+	show1.style.display = "none";
+}
